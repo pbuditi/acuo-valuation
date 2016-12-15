f14077390e753f9b011e86c4fe75a64d8bee2452
Hicham Medkouri <hicham.medkouri@gmail.com>
2016-12-15 10:08:02 +0800

    OW-268 added a FRA repository service to CRUD FRA entities

--- valuation-app/src/main/java/com/acuo/valuation/providers/acuo/TradeUploadServiceImpl.java
+++ valuation-app/src/main/java/com/acuo/valuation/providers/acuo/TradeUploadServiceImpl.java
@@ -1,9 +1,8 @@
 package com.acuo.valuation.providers.acuo;
 
-import com.acuo.persist.core.Neo4jPersistService;
 import com.acuo.persist.entity.FRA;
 import com.acuo.persist.entity.IRS;
-import com.acuo.persist.entity.Leg;
+import com.acuo.persist.services.FRAService;
 import com.acuo.persist.services.IRSService;
 import com.acuo.valuation.services.TradeUploadService;
 import com.acuo.valuation.utils.SwapExcelParser;
@@ -15,21 +14,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.inject.Inject;
 import java.io.InputStream;
-import java.util.Collections;
 
 @Slf4j
 public class TradeUploadServiceImpl implements TradeUploadService {
 
-    private Neo4jPersistService sessionProvider;
-
-    SwapExcelParser parser = new SwapExcelParser();
-
-    private IRSService irsService;
+    final SwapExcelParser parser = new SwapExcelParser();
+    private final IRSService irsService;
+    private final FRAService fraService;
 
     @Inject
-    public TradeUploadServiceImpl(Neo4jPersistService sessionProvider, IRSService irsService) {
-        this.sessionProvider = sessionProvider;
+    public TradeUploadServiceImpl(IRSService irsService, FRAService fraService) {
         this.irsService = irsService;
+        this.fraService = fraService;
     }
 
     public boolean uploadTradesFromExcel(InputStream fis) {
@@ -76,123 +72,30 @@ public class TradeUploadServiceImpl implements TradeUploadService {
 
     public void handleIRSRow(Row row) {
         IRS irs = parser.buildIRS(row);
-
-        //Iterable<IRS> list = sessionProvider.get().query(IRS.class, "MATCH (n:IRS {id:\"" + irs.getIrsId() + "\"}) RETURN n", Collections.emptyMap());
-
-        IRS queryIRS = irsService.findById(irs.getIrsId());
-        if (queryIRS != null) {
-            IRS existed = sessionProvider.get().load(IRS.class, queryIRS.getId(), 2);
-            log.debug("updating irs {} ", existed);
-            existed.setClearingDate(irs.getClearingDate());
-            existed.setMaturity(irs.getMaturity());
-            existed.setTradeType(irs.getTradeType());
-
-            log.debug("pay legs {} ", existed.getPayLegs() );
-            if (existed.getPayLegs() != null) {
-                for (Leg leg : existed.getPayLegs())
-                    sessionProvider.get().delete(leg);
-            }
-
-            log.debug("receive legs {} ", existed.getReceiveLegs() );
-            if (existed.getReceiveLegs() != null) {
-                for (Leg leg : existed.getReceiveLegs())
-                    sessionProvider.get().delete(leg);
-            }
-            existed.setPayLegs(irs.getPayLegs());
-            existed.setReceiveLegs(irs.getReceiveLegs());
-            sessionProvider.get().save(existed, 2);
-        } else {
-            log.debug("save irs {} into the DB", irs);
-            sessionProvider.get().save(irs, 2);
-        }
+        log.debug("parsed IRS {}", irs);
+        irs = irsService.createOrUpdate(irs);
+        log.debug("saved IRS {}", irs);
     }
 
     public void handleFRARow(Row row) {
         FRA fra = parser.buildFRA(row);
-        Iterable<FRA> list = sessionProvider.get().query(FRA.class, "MATCH (n:FRA {id:\"" + fra.getFraId() + "\"}) RETURN n", Collections.emptyMap());
-        if (list.iterator().hasNext()) {
-            //update case
-            FRA existed = sessionProvider.get().load(FRA.class, list.iterator().next().getId(), 2);
-            log.debug("updating fra {} ", existed);
-            existed.setClearingDate(fra.getClearingDate());
-            existed.setMaturity(fra.getMaturity());
-            existed.setCurrency(fra.getCurrency());
-            existed.setTradeType(fra.getTradeType());
+        log.debug("parsed IRS {}", fra);
+        fra = fraService.createOrUpdate(fra);
+        log.debug("saved IRS {}", fra);
 
-            if (fra.getPayLegs().size() > 0) {
-                if(existed.getPayLegs() != null) {
-                    for (Leg leg : existed.getPayLegs())
-                        sessionProvider.get().delete(leg);
-                }
-            } else if (fra.getReceiveLegs().size() > 0) {
-                if(existed.getReceiveLegs() != null) {
-                    for (Leg leg : existed.getReceiveLegs())
-                        sessionProvider.get().delete(leg);
-                }
-            }
-
-            existed.setPayLegs(fra.getPayLegs());
-            existed.setReceiveLegs(fra.getReceiveLegs());
-            sessionProvider.get().save(existed, 2);
-        } else {
-            log.debug("save fra {} into the DB", fra);
-            sessionProvider.get().save(fra, 2);
-        }
     }
 
     public void handleOISRow(Row row) {
         IRS irs = parser.buildOIS(row);
-        Iterable<IRS> list = sessionProvider.get().query(IRS.class, "MATCH (n:IRS {id:\"" + irs.getIrsId() + "\"}) RETURN n", Collections.emptyMap());
-        if (list.iterator().hasNext()) {
-            //update case
-            IRS existed = sessionProvider.get().load(IRS.class, list.iterator().next().getId(), 2);
-            log.debug("updating OIS {} ", existed);
-            existed.setClearingDate(irs.getClearingDate());
-            existed.setMaturity(irs.getMaturity());
-            existed.setTradeType(irs.getTradeType());
-
-            if (existed.getPayLegs() != null)
-                for (Leg leg : existed.getPayLegs())
-                    sessionProvider.get().delete(leg);
-            if (existed.getReceiveLegs() != null)
-                for (Leg leg : existed.getReceiveLegs())
-                    sessionProvider.get().delete(leg);
-            existed.setPayLegs(irs.getPayLegs());
-            existed.setReceiveLegs(irs.getReceiveLegs());
-            sessionProvider.get().save(existed, 2);
-        } else {
-            log.debug("save ois {} into the DB", irs);
-            sessionProvider.get().save(irs, 2);
-        }
+        log.debug("parsed IRS {}", irs);
+        irs = irsService.createOrUpdate(irs);
+        log.debug("saved IRS {}", irs);
     }
 
     public void handleIRSBilateralRow(Row row) {
         IRS irs = parser.buildIRSBilateral(row);
-        Iterable<IRS> list = sessionProvider.get().query(IRS.class, "MATCH (n:IRS {id:\"" + irs.getIrsId() + "\"}) RETURN n", Collections.emptyMap());
-        if (list.iterator().hasNext()) {
-            IRS existed = sessionProvider.get().load(IRS.class, list.iterator().next().getId(), 2);
-            log.debug("updating irs {} ", existed);
-            existed.setClearingDate(irs.getClearingDate());
-            existed.setMaturity(irs.getMaturity());
-            existed.setTradeType(irs.getTradeType());
-            log.debug("pay legs {} ", existed.getPayLegs() );
-            if (existed.getPayLegs() != null) {
-                for (Leg leg : existed.getPayLegs())
-                    sessionProvider.get().delete(leg);
-            }
-
-            log.debug("receive legs {} ", existed.getReceiveLegs() );
-            if (existed.getReceiveLegs() != null) {
-                for (Leg leg : existed.getReceiveLegs())
-                    sessionProvider.get().delete(leg);
-            }
-            existed.setPayLegs(irs.getPayLegs());
-            existed.setReceiveLegs(irs.getReceiveLegs());
-            sessionProvider.get().save(existed, 2);
-        } else {
-            log.debug("save irs {} into the DB", irs);
-            sessionProvider.get().save(irs, 2);
-        }
+        log.debug("parsed IRS {}", irs);
+        irs = irsService.createOrUpdate(irs);
+        log.debug("saved IRS {}", irs);
     }
-
 }
