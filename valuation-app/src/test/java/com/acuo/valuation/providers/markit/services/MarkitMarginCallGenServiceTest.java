41389e5ed1e0844f33450a69eec375ed2b0b8c8b
cantorfan <cantorfan@gmail.com>
2017-02-02 13:16:44 +0800

    fix unit test

--- valuation-app/src/test/java/com/acuo/valuation/providers/markit/services/MarkitMarginCallGenServiceTest.java
+++ valuation-app/src/test/java/com/acuo/valuation/providers/markit/services/MarkitMarginCallGenServiceTest.java
@@ -2,22 +2,39 @@ package com.acuo.valuation.providers.markit.services;
 
 import com.acuo.common.security.EncryptionModule;
 import com.acuo.common.util.GuiceJUnitRunner;
-import com.acuo.persist.entity.Agreement;
-import com.acuo.persist.entity.Portfolio;
-import com.acuo.persist.entity.Valuation;
+import com.acuo.persist.core.ImportService;
+import com.acuo.persist.entity.*;
 import com.acuo.persist.modules.*;
-import com.acuo.persist.services.AgreementService;
-import com.acuo.persist.services.PortfolioService;
-import com.acuo.persist.services.ValuationService;
+import com.acuo.persist.services.*;
 import com.acuo.valuation.modules.ConfigurationTestModule;
 import com.acuo.valuation.modules.EndPointModule;
 import com.acuo.valuation.modules.MappingModule;
 import com.acuo.valuation.modules.ServicesModule;
+import com.acuo.valuation.protocol.results.MarkitValuation;
+import com.acuo.valuation.protocol.results.PricingResults;
+import com.acuo.valuation.providers.acuo.Neo4jSwapService;
+import com.acuo.valuation.providers.acuo.Neo4jSwapServiceTest;
+import com.acuo.valuation.providers.markit.protocol.responses.MarkitValue;
 import com.acuo.valuation.services.MarginCallGenService;
+import com.acuo.valuation.services.PricingService;
+import com.opengamma.strata.basics.currency.Currency;
+import com.opengamma.strata.collect.result.Result;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceJUnitRunner.GuiceModules({ConfigurationTestModule.class,
@@ -30,26 +47,23 @@ import javax.inject.Inject;
         RepositoryModule.class,
         EndPointModule.class,
         ServicesModule.class})
-public class MarkitMarginCallGenServiceTest {
+public class MarkitMarginCallGenServiceTest extends Neo4jSwapServiceTest {
 
     @Inject
-    MarginCallGenService marginCallGenService;
-
-    @Inject
-    AgreementService agreementService;
-
-    @Inject
-    ValuationService valuationService;
-
-    @Inject
-    PortfolioService portfolioService;
+    MarginStatementService marginStatementService;
 
     @Test
-    public void testGeneareteMarginCall()
+    public void testGenMarginCall()
     {
-//        Agreement agreement = agreementService.findById("a12");
-//        Portfolio portfolio = portfolioService.findById("p12");
-//        Valuation valuation = valuationService.findById("2017/01/25-455773");
-//        marginCallGenService.geneareteMarginCall(agreement,portfolio,valuation);
+        testPriceSwapWithNoErrorReport();
+        String msId = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")) + "-a31";
+        MarginStatement marginStatement = marginStatementService.findById(msId);
+        Assert.assertNotNull(marginStatement);
+        for(MarginCall marginCall : marginStatement.getMarginCalls())
+        {
+            Assert.assertNotNull(marginCall.getExcessAmount());
+        }
     }
+
+
 }
