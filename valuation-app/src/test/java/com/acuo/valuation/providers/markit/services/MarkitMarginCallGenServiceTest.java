3342186470f670e109c69690d83ea846de6cb9e2
cantorfan <cantorfan@gmail.com>
2017-03-07 12:05:23 +0800

    479

--- valuation-app/src/test/java/com/acuo/valuation/providers/markit/services/MarkitMarginCallGenServiceTest.java
+++ valuation-app/src/test/java/com/acuo/valuation/providers/markit/services/MarkitMarginCallGenServiceTest.java
@@ -2,16 +2,24 @@ package com.acuo.valuation.providers.markit.services;
 
 import com.acuo.common.security.EncryptionModule;
 import com.acuo.common.util.GuiceJUnitRunner;
+import com.acuo.persist.entity.Agreement;
+import com.acuo.persist.entity.Portfolio;
+import com.acuo.persist.ids.PortfolioId;
 import com.acuo.persist.modules.*;
 import com.acuo.persist.services.MarginStatementService;
+import com.acuo.persist.services.PortfolioService;
 import com.acuo.valuation.modules.ConfigurationTestModule;
 import com.acuo.valuation.modules.EndPointModule;
 import com.acuo.valuation.modules.MappingModule;
 import com.acuo.valuation.modules.ServicesModule;
+import com.acuo.valuation.services.MarginCallGenService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceJUnitRunner.GuiceModules({ConfigurationTestModule.class,
@@ -27,19 +35,17 @@ import javax.inject.Inject;
 public class MarkitMarginCallGenServiceTest  {
 
     @Inject
-    MarginStatementService marginStatementService;
+    MarginCallGenService marginCallGenService;
+
+
 
     @Test
     public void testGenMarginCall()
     {
-//        testPriceSwapWithNoErrorReport();
-//        String msId = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")) + "-a31";
-//        MarginStatement marginStatement = marginStatementService.findById(msId);
-//        Assert.assertNotNull(marginStatement);
-//        for(MarginCall marginCall : marginStatement.getMarginCalls())
-//        {
-//            Assert.assertNotNull(marginCall.getExcessAmount());
-//        }
+//        PortfolioId portfolio = PortfolioId.fromString("p18");
+//        Set<PortfolioId> portfolios = new HashSet<PortfolioId>();
+//        portfolios.add(portfolio);
+//        marginCallGenService.marginCalls(portfolios, LocalDate.now());
     }
 
 
