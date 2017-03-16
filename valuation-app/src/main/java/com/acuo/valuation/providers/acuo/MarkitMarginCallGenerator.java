3bafc1e870d58c74a6d2405269654f0a1070dbf1
cantorfan <cantorfan@gmail.com>
2017-03-16 14:05:39 +0800

    for 313. we should generate IM instead of MarginCall

--- valuation-app/src/main/java/com/acuo/valuation/providers/acuo/MarkitMarginCallGenerator.java
+++ valuation-app/src/main/java/com/acuo/valuation/providers/acuo/MarkitMarginCallGenerator.java
@@ -145,7 +145,7 @@ public class MarkitMarginCallGenerator implements MarginCallGenService {
         marginAmount = deliverAmount + returnAmount;
 
 
-        MarginCall marginCall = new MarginCall(mcId,
+        MarginCall marginCall = new InitialMargin(mcId,
                 callDate,
                 marginType,
                 direction,
