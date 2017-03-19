f8926eca6b34ce26ef3f924b27eacfcdc807d9d0
cantorfan <cantorfan@gmail.com>
2017-03-19 17:23:09 +0800

    init push for Reuters

---
+++ valuation-app/src/main/java/com/acuo/valuation/providers/reuters/services/ReutersCall.java
@@ -0,0 +1,47 @@
+package com.acuo.valuation.providers.reuters.services;
+
+import com.acuo.common.http.client.Call;
+import com.acuo.common.http.client.CallBuilder;
+import com.acuo.common.http.client.ClientEndPoint;
+import okhttp3.*;
+
+public class ReutersCall extends CallBuilder<ReutersCall> {
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private final ClientEndPoint<ReutersEndPointConfig> client;
+
+    private final Request.Builder builder;
+
+    private ReutersCall(ClientEndPoint<ReutersEndPointConfig> client)
+    {
+        this.client = client;
+        ReutersEndPointConfig config = client.config();
+        HttpUrl uploadUrl = new HttpUrl.Builder()
+                .scheme(config.getScheme())
+                .host(config.getHost())
+                .port(config.getPort())
+                .addPathSegments(config.getUploadPath())
+                .addQueryParameter("apikey", config.getApikey())
+                .build();
+        builder = new Request.Builder()
+                .header("Content-Type", "application/json")
+                .header("X-TRACS-Position", config.getHeaderPosition())
+                .header("X-TRACS-ApplicationId", config.getHeaderApplicationId())
+                .url(uploadUrl);
+    }
+
+    public static ReutersCall of(ClientEndPoint<ReutersEndPointConfig> client)
+    {
+        return new ReutersCall(client);
+    }
+
+    public ReutersCall with(String key, String value) {
+        RequestBody body = RequestBody.create(JSON, value);
+        builder.post(body);
+        return this;
+    }
+
+    public Call create() {
+        Request request = builder.build();
+        return client.call(request, predicate);
+    }
+}
---
+++ valuation-app/src/main/java/com/acuo/valuation/providers/reuters/services/ReutersEndPointConfig.java
@@ -0,0 +1,65 @@
+package com.acuo.valuation.providers.reuters.services;
+
+import com.acuo.common.http.client.EndPointConfig;
+import lombok.Data;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.concurrent.TimeUnit;
+
+import static com.acuo.valuation.utils.PropertiesHelper.*;
+
+@Data
+public class ReutersEndPointConfig implements EndPointConfig {
+
+    private final String scheme;
+    private final String host;
+    private final int port;
+    private final String uploadPath;
+    private final String apikey;
+    private final String headerPosition;
+    private final String headerApplicationId;
+    private final int connectionTimeOut;
+    private final TimeUnit connectionTimeOutUnit;
+    private final boolean useProxy;
+
+    @Inject
+    public ReutersEndPointConfig(@Named(ACUO_VALUATION_REUTERS_SCHEME) String scheme,
+                                 @Named(ACUO_VALUATION_REUTERS_HOST) String host,
+                                 @Named(ACUO_VALUATION_REUTERS_PORT) int port,
+                                 @Named(ACUO_VALUATION_REUTERS_UPLOAD_PATH) String uploadPath,
+                                 @Named(ACUO_VALUATION_REUTERS_API_KEY) String apikey,
+                                 @Named(ACUO_VALUATION_REUTERS_HEADER_POSITION) String headerPosition,
+                                 @Named(ACUO_VALUATION_REUTERS_HEADER_APPLICATION_ID) String headerApplicationId,
+                                 @Named(ACUO_VALUATION_REUTERS_CONNECTION_TIMEOUT) int connectionTimeOut,
+                                 @Named(ACUO_VALUATION_REUTERS_USE_PROXY) String useProxy
+                                 )
+    {
+        this.scheme = scheme;
+        this.host = host;
+        this.port = port;
+        this.uploadPath = uploadPath;
+        this.apikey = apikey;
+        this.headerPosition = headerPosition;
+        this.headerApplicationId = headerApplicationId;
+        this.connectionTimeOut = connectionTimeOut;
+        this.connectionTimeOutUnit = TimeUnit.MILLISECONDS;
+        this.useProxy = Boolean.valueOf(useProxy);
+
+    }
+
+    @Override
+    public int connectionTimeOut() {
+        return connectionTimeOut;
+    }
+
+    @Override
+    public TimeUnit connectionTimeOutUnit() {
+        return connectionTimeOutUnit;
+    }
+
+    @Override
+    public boolean useLocalSocksProxy() {
+        return useProxy;
+    }
+}
