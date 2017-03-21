c730538df047d03ea2e09b243c2509e5c123738a
cantorfan <cantorfan@gmail.com>
2017-03-21 10:27:24 +0800

    OW-506

--- valuation-app/src/main/java/com/acuo/valuation/providers/reuters/services/ReutersServiceImpl.java
+++ valuation-app/src/main/java/com/acuo/valuation/providers/reuters/services/ReutersServiceImpl.java
@@ -1,22 +1,30 @@
 package com.acuo.valuation.providers.reuters.services;
 
+import com.acuo.collateral.transform.Transformer;
+import com.acuo.collateral.transform.TransformerContext;
 import com.acuo.common.http.client.ClientEndPoint;
+import com.acuo.common.model.assets.Assets;
 import com.acuo.valuation.services.ReutersService;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 public class ReutersServiceImpl implements ReutersService {
 
     private final ClientEndPoint<ReutersEndPointConfig> client;
+    private final Transformer<Assets> transformer;
 
     @Inject
-    public ReutersServiceImpl(ClientEndPoint<ReutersEndPointConfig> client)
+    public ReutersServiceImpl(ClientEndPoint<ReutersEndPointConfig> client, @Named("assets") Transformer<Assets> transformer)
     {
         this.client = client;
+        this.transformer = transformer;
     }
 
-    public String send(String value)
+    public String send(Assets assets)
     {
-        return ReutersCall.of(client).with("josn", value).create().send();
+        TransformerContext context = new TransformerContext();
+        String json = transformer.serialise(assets, context);
+        return ReutersCall.of(client).with("josn", json).create().send();
     }
 }
