2e3d509be530c29ada8e9665ffcb9505fe548068
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-03-22 15:52:13 +0000

    OW-453 added a cache on swap trade builder

--- valuation-app/src/test/java/com/acuo/valuation/util/MockServer.java
+++ valuation-app/src/test/java/com/acuo/valuation/util/MockServer.java
@@ -29,10 +29,10 @@ public class MockServer implements Runnable {
                     return new MockResponse().setResponseCode(200).setBody("key");
                 }
                 if (body.contains("key=key")) {
-                    return new MockResponse().setResponseCode(200).setBody(file("/markit/reports/markit-test-01.xml"));
+                    return new MockResponse().setResponseCode(200).setBody(file("/markit/reports/large.xml"));
                 }
                 if (body.contains("asof")) {
-                    return new MockResponse().setResponseCode(200).setBody(file("/markit/responses/markit-test-01.xml"));
+                    return new MockResponse().setResponseCode(200).setBody(file("/markit/responses/large.xml"));
                 }
                 return new MockResponse().setResponseCode(404);
             } catch (Exception e) {
