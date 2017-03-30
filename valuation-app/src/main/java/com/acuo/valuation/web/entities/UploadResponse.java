94b4ddca15a31e51afe47448dc35ee132953e516
Hicham Medkouri <hicham@anaxo.io>
2017-03-30 12:54:50 +0100

    OW-524 started to split the upload api in two wip

---
+++ valuation-app/src/main/java/com/acuo/valuation/web/entities/UploadResponse.java
@@ -0,0 +1,28 @@
+package com.acuo.valuation.web.entities;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UploadResponse {
+
+    private String txnID;
+    private List<Status> statuses;
+    private List<String> trades;
+
+    public enum StatusType {
+        success, failure
+    }
+
+    @Data
+    public static class Status {
+        private final StatusType status;
+        private final String remarks;
+
+        public Status(StatusType status, String remarks) {
+            this.status = status;
+            this.remarks = remarks;
+        }
+    }
+}
