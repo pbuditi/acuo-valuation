a5f78a9a0583090289ea4c5e612ab93f1f761868
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-04-07 22:51:59 +0100

    OW-520 enabled counterpart margin calls simulator

--- valuation-app/src/main/java/com/acuo/valuation/providers/acuo/calls/SimulationMarginCallBuilder.java
+++ valuation-app/src/main/java/com/acuo/valuation/providers/acuo/calls/SimulationMarginCallBuilder.java
@@ -1,20 +1,31 @@
 package com.acuo.valuation.providers.acuo.calls;
 
-import com.acuo.persist.entity.*;
+import com.acuo.persist.entity.Agreement;
+import com.acuo.persist.entity.MarginStatement;
+import com.acuo.persist.entity.TradeValue;
+import com.acuo.persist.entity.VariationMargin;
+import com.acuo.persist.entity.enums.StatementDirection;
+import com.acuo.persist.entity.enums.StatementStatus;
 import com.acuo.persist.ids.PortfolioId;
-import com.acuo.persist.services.*;
+import com.acuo.persist.services.AgreementService;
+import com.acuo.persist.services.CurrencyService;
+import com.acuo.persist.services.MarginCallService;
+import com.acuo.persist.services.MarginStatementService;
+import com.acuo.persist.services.PortfolioService;
+import com.acuo.persist.services.ValuationService;
 import com.acuo.valuation.providers.acuo.results.MarkitValuationProcessor;
-import com.acuo.valuation.services.MarginCallGenService;
+import com.opengamma.strata.basics.currency.Currency;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 
 @Slf4j
-public class SimulationMarginCallBuilder extends MarginCallGenerator implements MarginCallGenService, MarkitValuationProcessor.PricingResultProcessor {
+public class SimulationMarginCallBuilder extends MarkitMarginCallGenerator implements MarkitValuationProcessor.PricingResultProcessor {
 
     private MarkitValuationProcessor.PricingResultProcessor nextProcessor;
 
@@ -23,20 +34,22 @@ public class SimulationMarginCallBuilder extends MarginCallGenerator implements 
                                        PortfolioService portfolioService,
                                        MarginStatementService marginStatementService,
                                        AgreementService agreementService,
-                                       CurrencyService currencyService) {
+                                       CurrencyService currencyService,
+                                       MarginCallService marginCallService) {
         super(valuationService,
                 portfolioService,
                 marginStatementService,
                 agreementService,
-                currencyService);
+                currencyService,
+                marginCallService);
     }
 
     @Override
     public MarkitValuationProcessor.ProcessorItem process(MarkitValuationProcessor.ProcessorItem processorItem) {
-        log.info("processing item {}", processorItem);
+        log.info("processing markit valuation items");
         LocalDate date = processorItem.getResults().getDate();
         Set<PortfolioId> portfolioIds = processorItem.getPortfolioIds();
-        List<MarginCall> marginCalls = marginCalls(portfolioIds, date);
+        List<VariationMargin> marginCalls = createCalls(portfolioIds, date);
         processorItem.setSimulated(marginCalls);
         if (nextProcessor!= null)
             return nextProcessor.process(processorItem);
@@ -49,26 +62,19 @@ public class SimulationMarginCallBuilder extends MarginCallGenerator implements 
         this.nextProcessor = nextProcessor;
     }
 
-    public List<MarginCall> marginCalls(Set<PortfolioId> portfolioSet, LocalDate date) {
-        List<MarginCall> marginCalls = new ArrayList<MarginCall>();
-        double startVar = 1;
-        int i = 0;
-        for (PortfolioId portfolioId : portfolioSet) {
-            //for the random var
-            int index = (i + 1) / 2;
-            double rate = 0.2;
-            double var;
-            if (i % 2 == 0)
-                var = startVar + rate * index;
-            else
-                var = startVar - rate * index;
+    protected Supplier<StatementStatus> statementStatusSupplier() {
+        return () -> StatementStatus.Unrecon;
+    }
 
-            Portfolio portfolio = portfolioService.findById(portfolioId.toString());
-
-            Valuation valuation = valuationService.findById(date.format(dateTimeFormatter) + "-" + portfolio.getPortfolioId());
-            generateMarginCall((TradeValuation) valuation, date, CallStatus.Unrecon);
-
-        }
-        return marginCalls;
+    protected VariationMargin process(TradeValue value, Agreement agreement, LocalDate date, StatementStatus statementStatus, Map<Currency, Double> rates) {
+        TradeValue newValue = new TradeValue();
+        newValue.setCurrency(value.getCurrency());
+        newValue.setSource(value.getSource());
+        newValue.setValuation(value.getValuation());
+        VariationMargin variationMargin = new VariationMargin(value, statementStatus, agreement, rates);
+        StatementDirection direction = variationMargin.getDirection();
+        MarginStatement marginStatement = marginStatementService.getOrCreateMarginStatement(agreement, date, direction);
+        variationMargin.setMarginStatement(marginStatement);
+        return variationMargin;
     }
 }
