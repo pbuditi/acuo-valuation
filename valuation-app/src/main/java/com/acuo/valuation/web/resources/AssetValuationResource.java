baf0df5b11558a2f23fdfed5698d40043aba3cce
Hicham Medkouri <hicham@anaxo.io>
2017-04-18 14:30:12 +0100

    OW-561 changed the asset pricing services to accomodate with the latest changes on asset and asset value entities cash asset have now an asset value based on FX rates

--- valuation-app/src/main/java/com/acuo/valuation/web/resources/AssetValuationResource.java
+++ valuation-app/src/main/java/com/acuo/valuation/web/resources/AssetValuationResource.java
@@ -1,68 +1,55 @@
 package com.acuo.valuation.web.resources;
 
-import com.acuo.common.model.assets.Assets;
-import com.acuo.common.model.results.AssetValuation;
 import com.acuo.persist.entity.Asset;
+import com.acuo.persist.entity.AssetValue;
 import com.acuo.persist.services.AssetService;
-import com.acuo.valuation.providers.reuters.services.AssetsPersistService;
-import com.acuo.valuation.providers.reuters.services.ReutersService;
-import com.acuo.valuation.utils.AssetsBuilder;
+import com.acuo.valuation.jackson.AssetValueResult;
+import com.acuo.valuation.providers.acuo.assets.AssetPricingProcessor;
 import com.codahale.metrics.annotation.Timed;
+import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+
+import static java.util.stream.Collectors.toList;
 
 @Slf4j
-@Path("/swaps")
+@Path("/assets")
 @Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class AssetValuationResource {
 
-    private final ReutersService reutersService;
-    private final AssetsPersistService assetsPersistService;
+    private final AssetPricingProcessor assetPricingProcessor;
     private final AssetService assetService;
 
     @Inject
-    public AssetValuationResource(ReutersService reutersService,
-                                  AssetsPersistService assetsPersistService,
+    public AssetValuationResource(AssetPricingProcessor assetPricingProcessor,
                                   AssetService assetService) {
-        this.reutersService = reutersService;
-        this.assetsPersistService = assetsPersistService;
+        this.assetPricingProcessor = assetPricingProcessor;
         this.assetService = assetService;
     }
 
 
     @GET
-    @Path("/priceAsset/{id}")
+    @Path("/price/asset/{id}")
     @Timed
-    public String priceAssets(@PathParam("id") String assetId) throws Exception {
+    public Collection<AssetValueResult> priceAssets(@PathParam("id") String assetId) throws Exception {
         Asset asset = assetService.findById(assetId);
-        Assets assets = AssetsBuilder.buildAssets(asset);
-        List<Assets> assetsList = new ArrayList<>();
-        assetsList.add(assets);
-        List<AssetValuation> response = reutersService.send(assetsList);
-        response.stream().forEach(a -> assetsPersistService.persist(a));
-        return assetsList.size() + " asset(s) sent and " + response.size() + " valuation received";
+        final Collection<AssetValue> results = assetPricingProcessor.process(ImmutableList.of(asset));
+        return results.stream().map(AssetValueResult::new).collect(toList());
     }
 
     @GET
-    @Path("/priceAllAsset")
+    @Path("/price/all")
     @Timed
-    public String priceAllAssets() throws Exception {
-        List<Assets> assetsList = new ArrayList<>();
-        Iterable<Asset> assetIterable = assetService.findAll();
-        assetIterable.forEach(asset -> assetsList.add(AssetsBuilder.buildAssets(asset)));
-        List<AssetValuation> response = reutersService.send(assetsList);
-        response.stream().forEach(a -> assetsPersistService.persist(a));
-        return assetsList.size() + " asset(s) sent and " + response.size() + " valuation received";
+    public Collection<AssetValueResult> priceAllAssets() throws Exception {
+        Iterable<Asset> assets = assetService.findAll(1);
+        final Collection<AssetValue> results = assetPricingProcessor.process(assets);
+        return results.stream().map(AssetValueResult::new).collect(toList());
     }
-
 }
