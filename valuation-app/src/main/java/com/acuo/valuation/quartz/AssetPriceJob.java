7b83a0a65e7dc682dabc25328a7e202ba478ba40
Hicham Medkouri <hicham@anaxo.io>
2017-04-20 08:44:51 +0100

    OW-561 using the asset valuation processor in the quartz job

--- valuation-app/src/main/java/com/acuo/valuation/quartz/AssetPriceJob.java
+++ valuation-app/src/main/java/com/acuo/valuation/quartz/AssetPriceJob.java
@@ -1,43 +1,34 @@
 package com.acuo.valuation.quartz;
 
-import com.acuo.common.model.assets.Assets;
-import com.acuo.common.model.results.AssetValuation;
 import com.acuo.persist.entity.Asset;
+import com.acuo.persist.entity.AssetValue;
 import com.acuo.persist.services.AssetService;
-import com.acuo.persist.services.AssetValuationService;
-import com.acuo.valuation.providers.reuters.services.ReutersService;
-import com.acuo.valuation.utils.AssetsBuilder;
+import com.acuo.valuation.providers.acuo.assets.AssetPricingProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 @Slf4j
 public class AssetPriceJob implements Job {
 
-    private final ReutersService reutersService;
-    private final AssetValuationService assetsPersistService;
+    private final AssetPricingProcessor assetPricingProcessor;
     private final AssetService assetService;
 
     @Inject
-    public AssetPriceJob(ReutersService reutersService, AssetValuationService assetsPersistService, AssetService assetService) {
-        this.reutersService = reutersService;
-        this.assetsPersistService = assetsPersistService;
+    public AssetPriceJob(AssetPricingProcessor assetPricingProcessor, AssetService assetService) {
+        this.assetPricingProcessor = assetPricingProcessor;
         this.assetService = assetService;
     }
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         log.info("starting asset pricing job");
-        List<Assets> assetsList = new ArrayList<>();
-        Iterable<Asset> assetIterable = assetService.findAll();
-        assetIterable.forEach(asset -> assetsList.add(AssetsBuilder.buildAssets(asset)));
-        List<AssetValuation> response = reutersService.send(assetsList);
-        response.stream().forEach(a -> assetsPersistService.persist(a));
-        log.info("asset pricing job complete");
+        Iterable<Asset> assets = assetService.findAll(1);
+        final Collection<AssetValue> results = assetPricingProcessor.process(assets);
+        log.info("asset pricing job complete with {} responses", results.size());
     }
 }
