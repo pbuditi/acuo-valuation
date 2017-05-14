2a686035aa5b563c5395e307c79c6f1d77daaa12
Hicham Medkouri <hicham@anaxo.io>
2017-05-14 14:11:33 +0100

    OW-586 changed the Fx Rate relation to node and removed fxRates file form the docker list

--- valuation-app/src/main/java/com/acuo/valuation/providers/datascope/service/DataScopePersistServiceImpl.java
+++ valuation-app/src/main/java/com/acuo/valuation/providers/datascope/service/DataScopePersistServiceImpl.java
@@ -1,9 +1,9 @@
 package com.acuo.valuation.providers.datascope.service;
 
 import com.acuo.persist.entity.Asset;
-import com.acuo.persist.entity.FXRateRelation;
+import com.acuo.persist.entity.FXRate;
 import com.acuo.persist.services.AssetService;
-import com.acuo.persist.services.FXRateRelationService;
+import com.acuo.persist.services.FXRateService;
 import com.opengamma.strata.basics.currency.Currency;
 import com.opengamma.strata.basics.currency.CurrencyPair;
 import com.opengamma.strata.basics.currency.FxRate;
@@ -18,12 +18,12 @@ import java.util.List;
 @Slf4j
 public class DataScopePersistServiceImpl implements DataScopePersistService {
 
-    private final FXRateRelationService fxRateRelationService;
+    private final FXRateService fxRateService;
     private final AssetService assetService;
 
     @Inject
-    public DataScopePersistServiceImpl(FXRateRelationService fxRateRelationService, AssetService assetService) {
-        this.fxRateRelationService = fxRateRelationService;
+    public DataScopePersistServiceImpl(FXRateService fxRateService, AssetService assetService) {
+        this.fxRateService = fxRateService;
         this.assetService = assetService;
     }
 
@@ -36,15 +36,15 @@ public class DataScopePersistServiceImpl implements DataScopePersistService {
 
                 final Currency base = fxRate.getPair().getBase();
                 final Currency counter = fxRate.getPair().getCounter();
-                FXRateRelation fxRateRelation = fxRateRelationService.getOrCreate(base, counter);
+                FXRate fxRateRelation = fxRateService.getOrCreate(base, counter);
                 // workaround reuters wrong rate for JPYUSD=R
-                if (CurrencyPair.of(Currency.JPY, Currency.USD).equals(fxRate.getPair()))
-                    fxRateRelation.setFxRate(100d / fxRate.fxRate(fxRate.getPair()));
+                if (CurrencyPair.of(Currency.USD, Currency.JPY).equals(fxRate.getPair()))
+                    fxRateRelation.setValue(fxRate.fxRate(fxRate.getPair()) / 100);
                 else
-                    fxRateRelation.setFxRate(fxRate.fxRate(fxRate.getPair()));
+                    fxRateRelation.setValue(fxRate.fxRate(fxRate.getPair()));
                 fxRateRelation.setLastUpdate(lastUpdate);
 
-                fxRateRelationService.createOrUpdate(fxRateRelation);
+                fxRateService.createOrUpdate(fxRateRelation);
             }
         }
     }
@@ -78,13 +78,13 @@ public class DataScopePersistServiceImpl implements DataScopePersistService {
         boolean parser(String line) {
             try {
                 String[] columns = line.split(PATTERN);
-                String ccy1 = columns[0].substring(0, 3);
-                String ccy2 = columns[0].substring(3, 6);
-                this.rate = FxRate.parse(ccy1 + "/" + ccy2 + " " + columns[1]);
+                String counter = columns[0].substring(0, 3);
+                String base = columns[0].substring(3, 6);
+                this.rate = FxRate.parse(base + "/" + counter + " " + columns[1]);
                 this.lastUpdate = LocalDateTime.parse(columns[2], dateTimeFormatter);
                 return true;
             } catch (Exception e) {
-                log.error(e.getMessage(), e);
+                log.error(e.getMessage());
                 return false;
             }
         }
