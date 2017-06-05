f05e8718e508d39553f1d6e470e9d15d0f158fa7
cantorfan <cantorfan@gmail.com>
2017-06-05 14:34:50 +0800

    OW-509, set notional as parvalue

--- valuation-app/src/main/java/com/acuo/valuation/utils/AssetsBuilder.java
+++ valuation-app/src/main/java/com/acuo/valuation/utils/AssetsBuilder.java
@@ -30,7 +30,7 @@ public class AssetsBuilder {
 
         assets.setParValue(asset.getParValue());
         assets.setType(asset.getType());
-        assets.setParValue(asset.getParValue());
+        assets.setNotional(asset.getParValue());
         return assets;
     }
 }
