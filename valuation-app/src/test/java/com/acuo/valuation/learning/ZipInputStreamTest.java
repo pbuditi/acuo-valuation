5bdb7a810ec84278291f0e19f88042722eb570fa
cantorfan <cantorfan@gmail.com>
2017-06-07 16:57:18 +0800

    OW-610, fix unit test issue

--- valuation-app/src/test/java/com/acuo/valuation/learning/ZipInputStreamTest.java
+++ valuation-app/src/test/java/com/acuo/valuation/learning/ZipInputStreamTest.java
@@ -13,7 +13,7 @@ import java.util.zip.ZipInputStream;
 public class ZipInputStreamTest {
 
     @Rule
-    public ResourceFile excel = new ResourceFile("/excel/TradePortfolio.xlsx");
+    public ResourceFile excel = new ResourceFile("/excel/TradePortfolio18-05-17v2-NPV.xlsx");
 
     @Test
     public void testCreateZipStreamFromString() throws IOException {
