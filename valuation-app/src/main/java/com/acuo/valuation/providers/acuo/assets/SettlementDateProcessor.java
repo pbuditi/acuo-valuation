39ad1427cc4ec53d8cda8ad714c84e18423ee0ba
cantorfan <cantorfan@gmail.com>
2017-07-06 21:41:49 +0800

    OW-676, add persist

--- valuation-app/src/main/java/com/acuo/valuation/providers/acuo/assets/SettlementDateProcessor.java
+++ valuation-app/src/main/java/com/acuo/valuation/providers/acuo/assets/SettlementDateProcessor.java
@@ -2,12 +2,16 @@ package com.acuo.valuation.providers.acuo.assets;
 
 import com.acuo.common.model.assets.Assets;
 import com.acuo.common.model.results.AssetSettlementDate;
+import com.acuo.persist.entity.Asset;
+import com.acuo.persist.entity.SettlementDate;
 import com.acuo.persist.services.AssetService;
 import com.acuo.valuation.providers.reuters.services.SettlementDateService;
 import com.acuo.valuation.utils.AssetsBuilder;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -17,12 +21,16 @@ public class SettlementDateProcessor {
 
     private final AssetService assetService;
     private final SettlementDateService settlementDateService;
+    private final com.acuo.persist.services.SettlementDateService service;
+
 
     @Inject
-    public SettlementDateProcessor(AssetService assetService, SettlementDateService settlementDateService)
+    public SettlementDateProcessor(AssetService assetService, SettlementDateService settlementDateService,
+                                   com.acuo.persist.services.SettlementDateService service)
     {
         this.assetService = assetService;
         this.settlementDateService = settlementDateService;
+        this.service = service;
     }
 
     public void process()
@@ -32,6 +40,29 @@ public class SettlementDateProcessor {
         log.info("assets to send :" + assets.toString());
         List<AssetSettlementDate> assetSettlementDates = settlementDateService.send(assets);
         log.info("settlementDate received :" + assetSettlementDates);
+        for(AssetSettlementDate assetSettlementDate : assetSettlementDates)
+        {
+            Asset asset = assetService.find(assetSettlementDate.getAssetId());
+            if(asset.getSettlementDate() == null)
+            {
+                SettlementDate root = new SettlementDate();
+                root.setSettlementDateId(asset.getAssetId() + "sd");
+                asset.setSettlementDate(root);
+                assetService.save(asset);
+            }
+            asset = assetService.find(assetSettlementDate.getAssetId());
+            SettlementDate root = service.find(asset.getSettlementDate().getSettlementDateId());
+            if(root.getSettlementDates() == null)
+                root.setSettlementDates(new HashSet<>());
+
+            SettlementDate child = new SettlementDate();
+            child.setQueryDateTime(LocalDateTime.now());
+            child.setSettlementDateId(root.getSettlementDateId() + root.getSettlementDates().size());
+            child.setSettlementDate(assetSettlementDate.getSettlementDate());
+            root.getSettlementDates().add(child);
+            service.save(root);
+        }
+
 
     }
 }
