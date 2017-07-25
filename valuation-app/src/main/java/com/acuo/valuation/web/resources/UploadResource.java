70b3f273aec9d38756d974f6557b1ffd9b560dff
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-07-25 21:31:19 +0100

    OW-705 handling multiple file upload

--- valuation-app/src/main/java/com/acuo/valuation/web/resources/UploadResource.java
+++ valuation-app/src/main/java/com/acuo/valuation/web/resources/UploadResource.java
@@ -11,6 +11,8 @@ import com.acuo.valuation.web.entities.UploadResponse;
 import com.codahale.metrics.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.parboiled.common.ImmutableList;
 
 import javax.inject.Inject;
@@ -23,8 +25,14 @@ import javax.ws.rs.core.Response;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static javax.ws.rs.core.Response.Status.CREATED;
 
 @Slf4j
@@ -49,12 +57,25 @@ public class UploadResource {
     @Produces({MediaType.APPLICATION_JSON})
     @Path("/v1")
     @Timed
-    public Response uploadV1(@MultipartForm UploadForm entity) throws IOException {
-        ByteArrayInputStream fis = new ByteArrayInputStream(entity.getFile());
-        log.info("start uploading trade file ");
-        List<Portfolio> portfolios = tradeUploadService.fromExcelWithValues(fis);
+    public Response uploadV1(MultipartFormDataInput input/*@MultipartForm UploadForm entity*/) throws IOException {
+        final Map<String, List<InputPart>> formDataMap = input.getFormDataMap();
+        List<InputPart> parts = formDataMap.get("file");
+        log.info("start uploading trade {} file(s)", parts.size());
+        Set<Portfolio> portfolios = parts.stream()
+                .map(part -> {
+                    try {
+                        return part.getBody(byte[].class, null);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .map(ByteArrayInputStream::new)
+                .flatMap(fis -> tradeUploadService.fromExcelWithValues(fis).stream())
+                .collect(toSet());
         LocalDate valuationDate = LocalDateUtils.valuationDate();
-        final MarginCallResponse  response = portfolioManager.split(portfolios, valuationDate);
+        final MarginCallResponse  response = portfolioManager.split(new ArrayList<>(portfolios), valuationDate);
         log.info("uploading trade file complete, loaded {} portfolios", portfolios.size());
         return Response.status(CREATED).entity(response).build();
     }
